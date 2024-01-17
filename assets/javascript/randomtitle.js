7192c3fa71c9f4d6ce8637d156f0f6ca021712b8
curse <77826401+movingfeds@users.noreply.github.com>
2024-01-17 06:48:10 -0800

    main

---
+++ assets/javascript/randomtitle.js
@@ -0,0 +1,5 @@
+// randomowy tytuł strony
+
+  var titles = ['i win', 'cursedsec', 'cursedsec on top', 'yall nns', 'too easy for cursed']
+  var title = titles[Math.floor(Math.random() * titles.length)]
+  document.title = title
