e0407fafeeb71bf452594dd05eab1628b2f785a9
curse <77826401+movingfeds@users.noreply.github.com>
2024-06-11 09:46:57 -0700

    Delete assets directory

--- assets/javascript/randomtitle.js
+++
@@ -1,5 +0,0 @@
-// randomowy tytuł strony
-
-  var titles = ['i win', 'cursedsec', 'cursedsec on top', 'yall nns', 'too easy for cursed']
-  var title = titles[Math.floor(Math.random() * titles.length)]
-  document.title = title
